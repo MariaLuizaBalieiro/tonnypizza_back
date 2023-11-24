cbb5717e190b7e6d6facf3290cd3b4af24c437bd
Maria Luiza <maria.luiza.costa.balieiro@gmail.com>
2023-11-24 12:20:51 -0300

    em andamento

---
+++ categorias.js
@@ -0,0 +1,6 @@
+var categorias = {
+    pizzas:
+    [
+
+    ]
+}
---
+++ usuariosPerfil.js
@@ -0,0 +1,40 @@
+var usuariosPerfil = {
+    clientes:
+    [
+    {
+       nome: 'Tamires Fernandes',
+       endereco: 'Jardim angelica, carapicuiba- sp',
+       telefone:'99252-1426',
+       email:'tamires.fernandes@gmail.com',
+       senha: 'euamomeunamorado123'
+    }, 
+    {
+        nome: 'Luana Maga',
+        endereco: 'Rua dos quinto dos infernos, jandira- SP',
+        telefone:'6666-6565',
+        email:'luana.maga@gmail.com',
+        senha: 'EUodeioOAescola'
+     }, 
+     {
+        nome: 'Kainão Braga',
+        endereco: 'Rua onde judas perdeu as botas, jandira- SP',
+        telefone:'993467-9090',
+        email:'kainãobraga@gmail.com',
+        senha: 'aaaaaaaaaaaaaaaaa1234'
+     },
+     {
+        nome: 'Maria Luiza Balieiro',
+        endereco: 'Rua dos doidos, barueri- sp ',
+        telefone:'95088-1414',
+        email:'malu.balieiro@gmail.com',
+        senha: 'eunãoaguentomais28'
+     },
+     {
+        nome: 'Alice Zeurgo',
+        endereco: 'rua dos psiquiatras, jandira- sp ',
+        telefone:'99289-8672',
+        email:'aliceZeurgo@gmail.com',
+        senha: 'aaliceamaoeduardo'
+     }
+    ]
+}
